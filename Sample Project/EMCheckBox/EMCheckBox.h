925e22023fb6a1fc3ee3b35320104f193d5f3491
liujianlin <liujianlin@yiqixxx.com>
2016-07-27 13:37:10 +0800

    add framework

---
+++ Sample Project/EMCheckBox/EMCheckBox.h
@@ -0,0 +1,19 @@
+//
+//  EMCheckBox.h
+//  EMCheckBox
+//
+//  Created by liujianlin on 16/7/27.
+//  Copyright © 2016年 Boris Emorine. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for EMCheckBox.
+FOUNDATION_EXPORT double EMCheckBoxVersionNumber;
+
+//! Project version string for EMCheckBox.
+FOUNDATION_EXPORT const unsigned char EMCheckBoxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EMCheckBox/PublicHeader.h>
+
+
